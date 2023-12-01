ff443aca37a95a987e19414ab15807419acdbb03
daniel <d.danielprince2003@gmail.com>
2023-12-01 18:09:19 +0530

    feat:changed the ip method

--- extractor.py
+++ extractor.py
@@ -19,7 +19,7 @@ except :
 
 index = input("Enter the index name for pivot table (Case Sensitive) : ")
 columns = input("Enter the column name for pivot table (Case Sensitive) : ")
-values = input("Enter the Value name for pivot table (Case Sensitive) : ")
+values = input("Enter pivot table (Case Sensitive) : ")
 
 # Ask the user to choose a function for the pivot table (Sum / Average / Both)
 
