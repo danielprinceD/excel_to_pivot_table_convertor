1226c9aa3eb946ac17c48a3b79be3d3be755b81d
daniel <d.danielprince2003@gmail.com>
2023-12-20 17:55:46 +0530

    feat:Completed Extractor.py

--- extractor.py
+++ extractor.py
@@ -6,7 +6,6 @@ import sys,os
 xl_file = input("Enter the Excel file name : ")
 xl_file=str(xl_file) 
 
-# Try to read the Excel file, exit if the file is not found
 
 try : 
     df = pandas.read_excel(f'{xl_file}')
@@ -14,19 +13,17 @@ except :
     print("Enter Valid FileName")
     exit()
     
-# Ask the user to input names for pivot table index, columns, and values
 
 
 index = input("Enter the index name for pivot table (Case Sensitive) : ")
 columns = input("Enter the column name for pivot table (Case Sensitive) : ")
 values = input("Enter the Value name for pivot table (Case Sensitive) : ")
 
-# Ask the user to choose a function for the pivot table (Sum / Average / Both)
 
 function = str(input("Sum / Average / both : ")).lower()
 
 
-# Convert user input to valid pivot table aggregation function
+
 
 if function == 'sum' : 
     function = 'sum'
@@ -37,13 +34,11 @@ elif function == 'both' :
 else : 
     print("Enter Valid Option") 
 
-# Remove any trailing whitespaces from the user input
 
 index = str(index).rstrip()
 columns = str(columns).rstrip()
 values = str(values).rstrip()
 
-# Try to create a pivot table, exit if the pivot table values are invalid
 
 try : 
     pt = df.pivot_table(index=index , columns=columns ,values =values ,aggfunc=function)
@@ -52,14 +47,11 @@ except :
     exit()
 
 
-# Define the file path for saving the pivot table CSV file
 
 file_name = os.path.dirname((sys.executable))
 
-# Convert the pivot table to CSV and save it to the specified file path
 
 f_path = os.path.join(file_name,f'pivot-table{xl_file}')
 
-# Print a success message with the file path
 
 convert_pivot = pt.to_csv(f_path)
