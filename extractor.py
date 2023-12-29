ad5fc68374b174b46b8dda554e33ffa5bd9e58df
daniel <d.danielprince2003@gmail.com>
2023-12-29 17:50:17 +0530

    feat:Completed Extractor.py

--- extractor.py
+++ extractor.py
@@ -39,9 +39,9 @@ else :
 
 # Remove any trailing whitespaces from the user input
 
-index = str(index).rstrip()
-columns = str(columns).rstrip()
-values = str(values).rstrip()
+index = str(index)
+columns = str(columns)
+values = str(values)
 
 # Try to create a pivot table, exit if the pivot table values are invalid
 
